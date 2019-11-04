d364a9ae0842a9f69a28913b32d72e71dfc47590
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2019-11-04 12:30:13 +0100

    UPDATED: Prototype Maxim

---
+++ Assets/Prototypes/Maxim/DungeonGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    public List<GameObject> Rooms;
+
+    private int _spacing = 18;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rooms.Add(Instantiate(Rooms[0], new Vector3(0, 0, 0), Quaternion.identity));
+        Rooms.Add(Instantiate(Rooms[0], new Vector3(-_spacing, 0, 0), Quaternion.identity));
+        Rooms.Add(Instantiate(Rooms[0], new Vector3(_spacing, 0, 0), Quaternion.identity));
+        Rooms.Add(Instantiate(Rooms[0], new Vector3(0, 0, _spacing), Quaternion.identity));
+        Rooms.Add(Instantiate(Rooms[0], new Vector3(0, 0, -_spacing), Quaternion.identity));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
