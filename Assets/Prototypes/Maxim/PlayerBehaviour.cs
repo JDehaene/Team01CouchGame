9c15b0c8430b6c6d0dc237e6076b5fc67c1ac287
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2019-11-11 14:30:36 +0100

    UPDATED: Character movement

---
+++ Assets/Prototypes/Maxim/PlayerBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float _velocity = 5;
+    [SerializeField]
+    private float _turnSpeed = 10;
+
+    private Vector2 _input;
+    private float _angle;
+    private Quaternion _targetRotation;
+    private Transform _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        GetInput();
+
+        if (Mathf.Abs(_input.x) < 0.2 && Mathf.Abs(_input.y) < 0.2) return;
+
+        CalculateDirection();
+        Rotate();
+        Move();
+
+        Debug.Log(_angle);
+    }
+
+    private void Move()
+    {
+        transform.position += transform.forward * _velocity * Time.deltaTime;
+    }
+
+    private void Rotate()
+    {
+        _targetRotation = Quaternion.Euler(0, _angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, _targetRotation, _turnSpeed * Time.deltaTime);
+    }
+
+    private void CalculateDirection()
+    {
+        _angle = Mathf.Atan2(_input.x, _input.y);
+        _angle = Mathf.Rad2Deg * _angle;
+        //_angle = _camera.eulerAngles.y;
+    }
+
+    private void GetInput()
+    {
+        _input.x = Input.GetAxisRaw("Horizontal");
+        _input.y = Input.GetAxisRaw("Vertical");
+    }
+}
