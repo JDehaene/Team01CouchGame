bff516c766b876cc7d74bd7359da6234f7fca7fe
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2019-11-11 20:55:28 +0100

    UPDATED: Working dungeon generating, working on camera

--- Assets/Prototypes/Maxim/CameraBehaviour.cs
+++ Assets/Prototypes/Maxim/CameraBehaviour.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Camera _camera;
+    private DungeonGenerator _dungeonGenerator;
+
+    private int _roomIndex = 0;
+    private bool _goNextRoom = false;
+
     void Start()
     {
-        
+        _camera = Camera.main;
+        _dungeonGenerator = transform.GetChild(1).GetComponent<DungeonGenerator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (_goNextRoom)
+        {
+            GameObject waypoint = _dungeonGenerator.RoomList[_roomIndex].gameObject;
+            Vector3.Lerp(_camera.transform.position, waypoint.transform.position, 5 * Time.deltaTime);
+
+            if (Vector3.Distance(_camera.transform.position, waypoint.transform.position) < 0.25f)
+            {
+                _goNextRoom = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _goNextRoom = true;
+            _roomIndex++;
+        }
     }
 }
--- Assets/Prototypes/Maxim/DungeonGenerator.cs
+++ Assets/Prototypes/Maxim/DungeonGenerator.cs
@@ -1,21 +1,115 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
     public List<GameObject> Rooms;
+    private List<Vector3> _positionList = new List<Vector3>();
+
+    public List<GameObject> RoomList = new List<GameObject>();
 
     private int _spacing = 18;
+    private int _maxRooms = 10;
+    private int _randomNumber;
+    private int _lastNumber = 5;
+
+    private Vector3 _oldPosition = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        Rooms.Add(Instantiate(Rooms[0], new Vector3(0, 0, 0), Quaternion.identity));
-        Rooms.Add(Instantiate(Rooms[0], new Vector3(-_spacing, 0, 0), Quaternion.identity));
-        Rooms.Add(Instantiate(Rooms[0], new Vector3(_spacing, 0, 0), Quaternion.identity));
-        Rooms.Add(Instantiate(Rooms[0], new Vector3(0, 0, _spacing), Quaternion.identity));
-        Rooms.Add(Instantiate(Rooms[0], new Vector3(0, 0, -_spacing), Quaternion.identity));
+        _positionList.Add(_oldPosition);
+
+        for (int roomIndex = 0; roomIndex < _maxRooms; roomIndex++)
+        {
+            //eerste generate (na spawn)
+            _randomNumber = UnityEngine.Random.Range(-2, 3);
+            Debug.Log("Random Number: " + _randomNumber);
+
+            while (_randomNumber == _lastNumber || _randomNumber == 0)
+            {
+                _randomNumber = UnityEngine.Random.Range(-2, 3);
+                Debug.Log("Random Number: " + _lastNumber);
+            }         
+
+            Vector3 position = GenerateRandomPosition(_randomNumber);
+
+            _positionList.Add(position);
+
+            for (int positionIndex = 0; positionIndex < _positionList.Count; positionIndex++)
+            {
+                if (position + _oldPosition == _positionList[positionIndex])
+                {
+                    do
+                    {
+                        //eerste generate (na spawn)
+                        _randomNumber = UnityEngine.Random.Range(-2, 3);
+                        Debug.Log("Random Number: " + _randomNumber);
+
+                        while (_randomNumber == _lastNumber || _randomNumber == 0)
+                        {
+                            _randomNumber = UnityEngine.Random.Range(-2, 3);
+                            Debug.Log("Random Number: " + _lastNumber);
+                        }
+
+                        position = GenerateRandomPosition(_randomNumber);
+                    } while (position == _positionList[positionIndex]);
+                }
+
+                Debug.Log("List:  n" + positionIndex + ": pos" + position);
+            }
+
+            if (_randomNumber < 0)
+            {
+                _lastNumber = Mathf.Abs(_randomNumber);
+            }
+            else
+            {
+                _lastNumber = -_randomNumber;
+            }
+
+            Debug.Log("Last Number: " + _lastNumber);
+
+            GameObject room;
+
+            if (roomIndex + 1 == _maxRooms)
+            {
+                room = Instantiate(Rooms[1], _oldPosition + position, Quaternion.identity);
+            }
+            else
+            {
+                room = Instantiate(Rooms[0], _oldPosition + position, Quaternion.identity);
+            }
+
+            RoomList.Add(room);
+          
+            _oldPosition = room.transform.position;
+
+            Debug.Log("New room Position: " + room.transform.position);
+            Debug.Log("Old Position: " + _oldPosition);         
+        }
+    }
+
+    private Vector3 GenerateRandomPosition(int number)
+    {
+        if (number == -2)
+        {
+            return new Vector3(-_spacing, 0, 0);
+        }
+        else if (number == 2)
+        {
+            return new Vector3(_spacing, 0, 0);
+        }
+        else if (number == 1)
+        {
+            return new Vector3(0, 0, _spacing);
+        }
+        else
+        {
+            return new Vector3(0, 0, -_spacing);
+        }
     }
 
     // Update is called once per frame
