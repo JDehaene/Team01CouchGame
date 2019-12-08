9501a291c0264ce7e6c6eb7c7e3f70241d550251
JDehaene <43540756+JDehaene@users.noreply.github.com>
2019-12-08 17:49:08 +0100

    Added extra condition
    -When ghosts kill player in last room, ghost win
    otherwise player wins
    -Added canvas in Maxim Level

--- Assets/Prototypes/Maxim/Scripts/GameConditionManager.cs
+++ Assets/Prototypes/Maxim/Scripts/GameConditionManager.cs
@@ -1,28 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameConditionManager : MonoBehaviour
 {
     private GameObject[] _activePlayers;
+    private GameObject[] _activeGhosts;
     private Sacrifice _sacrificeBehaviour;
+    private Generator _generator;
+    [SerializeField] private Text _winnerText;
+    private float _timer;
+    [SerializeField] private float _restartGameTimer;
+
     private void Update()
     {
         CheckPlayersAlive();
+
+        if (_generator.CameraIndex <= _generator.NumberOfRooms)
+        {
+            CheckEndFight();
+        }
     }
 
     private void CheckPlayersAlive()
     {
         _activePlayers = GameObject.FindGameObjectsWithTag("Player");
-
+        _activeGhosts = GameObject.FindGameObjectsWithTag("Ghost");
         if (_activePlayers.Length < 1)
-            EndGame();
-    }
-    private void CheckSacrifice()
-    {
+        {
+            _timer += Time.deltaTime;
+            CheckEndFight();
+            if(_timer > _restartGameTimer)
+                EndGame();
 
+        }
     }
+    
+    private void CheckEndFight()
+    {
+        if (_activePlayers.Length > _activeGhosts.Length)
+            _winnerText.text = "Player wins!";
+        else
+            _winnerText.text = "Ghosts win!";
+    }
+
     private void EndGame()
     {
         SceneManager.LoadScene("CharacterSelection");
