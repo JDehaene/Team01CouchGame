5ae22ff89440a5d80db9141bf68551a33777e37c
DESKTOP-4TR8F29\Guust <diedriek@gmail.com>
2019-12-18 16:57:26 +0100

    added new pickups
    - added model: small potion
    - added model: medium potion
    - added model: big potion
    - added prefab: heal, speed buf and updated prefab: itemspwamer

---
+++ Assets/Prototypes/Maxim/Scripts/ItemRotationScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRotationScript : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        transform.Rotate(0, _speed * Time.deltaTime ,0);
+    }
+
+}
