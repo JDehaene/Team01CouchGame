e5ff4233fe2b6cc5dd2a6feba05c157e79a4d5d7
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2019-12-29 13:01:33 +0100

    ADDED: Moveable wall (extra obstacle for sacrifise room)

---
+++ Assets/Prototypes/Maxim/Scripts/MovingWall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWall : MonoBehaviour
+{
+    private float _timer;
+
+    [SerializeField] private float _amplitude = 1;
+    [SerializeField] private float _frequency = 1;
+
+    void Update()
+    {
+        float y = _amplitude * Mathf.Sin(_timer * _frequency);
+        this.transform.localScale = new Vector3(1, Mathf.Abs(y), 1);
+
+        _timer += Time.deltaTime;
+    }
+}
