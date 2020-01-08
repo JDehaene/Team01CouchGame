fd4c8e1e67c15b34b0dd3abd0f8e814e97781edd
DESKTOP-4TR8F29\Guust <diedriek@gmail.com>
2020-01-08 18:44:29 +0100

    main menu
    - new scene: main menu
    - new script: ui

---
+++ Assets/Prototypes/Maxim/Scripts/UiManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+
+    private bool _gamePaused = false, _help =false, _options= false;
+
+    [SerializeField] private GameObject _pauseScreen, _helpScreen, _optionScreen;
+
+    private void Start()
+    {
+        _pauseScreen.active = false;
+        _optionScreen.active = false;
+        _helpScreen.active = false;
+        Time.timeScale = 1.0f;
+    }
+
+    void Update()
+    {
+        if (!_gamePaused && Input.GetButtonDown("Pause"))
+        {
+            PauseGame();
+        }
+        else if (_gamePaused && Input.GetButtonDown("Pause") || _gamePaused && Input.GetButtonDown("B") && !_options && !_help)
+        {
+            ResumeGame();
+        }
+
+        if(_help && Input.GetButtonDown("B") || _options && Input.GetButtonDown("B"))
+        {
+            PauseGame();
+        }
+
+
+    }
+
+    public void MainMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void RestartGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(1);
+    }
+
+    //ui options with canvas overlays
+    public void PauseGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0.0f;
+        _pauseScreen.active = true;
+        _gamePaused = true;
+        _helpScreen.active = false;
+        _optionScreen.active = false;
+        _options = false;
+        _help = false;
+    }
+
+    public void ResumeGame()
+    {
+        _gamePaused = false;
+        _pauseScreen.active = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1.0f;
+    }
+
+    public void Options()
+    {
+        _options = true;
+        _pauseScreen.active = false;
+        _optionScreen.active = true;
+    }
+
+    public void Help()
+    {
+        _help = true;
+        _pauseScreen.active = false;
+        _helpScreen.active = true;
+    }
+
+}
