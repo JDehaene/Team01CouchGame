a3be50294943b9ab99aaf21796baad132f822333
DESKTOP-4TR8F29\Guust <diedriek@gmail.com>
2020-01-11 00:51:13 +0100

    finilised startscreen ui

---
+++ Assets/Prototypes/Maxim/Scripts/StartScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    [SerializeField] private GameObject _startScreenUI, _mainMenuUI;
+
+    private void Start()
+    {
+        _mainMenuUI.active = false;
+    }
+
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            _mainMenuUI.active = true;
+            _startScreenUI.active = false;
+        }
+    }
+}
